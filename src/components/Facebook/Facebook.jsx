087b565b20e2d4fb3230d9373fb1228494970308
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-06 15:39:05 -0700

    Installed web vitals

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -1,8 +1,5 @@
 import * as React from 'react'
 import FacebookLogin from 'react-facebook-login' //External library
-import axios from 'axios'
-
-const backendUrl="http://localhost:3001/"
 
 export default function Facebook({responseFacebook}) {
   
@@ -11,7 +8,7 @@ export default function Facebook({responseFacebook}) {
   }
 
   return (
-    <div className={FacebookLogin}>{<FacebookLogin
+    <div className="FacebookLogin">{<FacebookLogin
       appId="3192124317669140"
       autoLoad={false}
       fields="name,email,picture"
