f13057209fb8dee1b594a39e251ec926986be06d
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-06 12:04:56 -0700

    Connecting Frontend and Backend
    Will look into pull requests after lunch (sorry!)

--- dungeon-delver-frontend/src/components/Facebook/Facebook.jsx
+++ dungeon-delver-frontend/src/components/Facebook/Facebook.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import * as React from 'react'
 import FacebookLogin from 'react-facebook-login' //External library
+import axios from 'axios'
+
+const backendUrl="http://localhost:3001/"
 
 export default function Facebook() {
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [userId, setUserId] = React.useState("");
-  const [name, setUserName] = React.useState("");
+  const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [picture, setPicture] = React.useState();
 
   const componentClicked = () => {
-    console.log('clicked');
   }
 
+  const postData = async (response) => {
+    try {
+      const userData = await axios.post(`${backendUrl}/user`, {userData: response})
+      setLoggedIn(true);
+      setUserId(response.userID);
+      setName(response.name);
+      setEmail(response.email);
+      setPicture(response.picture.data.url);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  
   const responseFacebook = response => {
     console.log(response);
+    postData(response);
   }
 
   let fbContent = null;
 
   if(loggedIn) {
     //do things when logged in
+    fbContent=(
+      <div>
+        <img src={picture} alt={name} />
+        <h2>Welcome {name}!</h2>
+        Email: {email}
+      </div>
+    )
   }
   else {
     fbContent = (<FacebookLogin
