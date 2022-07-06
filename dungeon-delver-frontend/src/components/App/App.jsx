ab7d9a295cf742bebbd2b8733f003be9208c8cf1
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-06 15:22:02 -0700

    Refactored code to put states in the App.jsx file so they can be passed down to every prop that needs them in the future
    I forgot to make a pull request before starting these edits: will do for the next one

--- dungeon-delver-frontend/src/components/App/App.jsx
+++ dungeon-delver-frontend/src/components/App/App.jsx
@@ -1,19 +1,50 @@
+import * as React from 'react'
 import './App.css';
 import Facebook from '../Facebook/Facebook';
 
-function App() {
+import axios from 'axios'
+const backendUrl="http://localhost:3001/"
 
-  return (
-    <div className="App">
-      <h1>
-        Dungeon Delver
-      </h1>
-      <p>
-        To get started, authenticate with Facebook.
-      </p>
-      <Facebook />
-    </div>
-  );
+
+
+function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [userId, setUserId] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [picture, setPicture] = React.useState();
+
+  const responseFacebook = async (response) => {
+    try {
+      const userData = await axios.post(`${backendUrl}user`, {userData: response})
+      // Update state variable holding current user
+      setLoggedIn(true);
+      setUserId(response.userID);
+      setName(response.name);
+      setEmail(response.email);
+      setPicture(response.picture.data.url);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  if(!loggedIn) {
+    return (
+      <div className="App">
+        <h1>
+          Dungeon Delver
+        </h1>
+        <p>
+          To get started, authenticate with Facebook.
+        </p>
+        <Facebook responseFacebook={responseFacebook}/>
+      </div>
+    );
+  }
+  else {
+    return <div>{name}</div>;
+  }
 }
 
 export default App;
--- dungeon-delver-frontend/src/components/Facebook/Facebook.jsx
+++ dungeon-delver-frontend/src/components/Facebook/Facebook.jsx
@@ -4,58 +4,18 @@ import axios from 'axios'
 
 const backendUrl="http://localhost:3001/"
 
-export default function Facebook() {
-  const [loggedIn, setLoggedIn] = React.useState(false);
-  const [userId, setUserId] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [picture, setPicture] = React.useState();
+export default function Facebook({responseFacebook}) {
+  
 
   const componentClicked = () => {
   }
 
-  const postData = async (response) => {
-    try {
-      const userData = await axios.post(`${backendUrl}user`, {userData: response})
-      // Update state variable holding current user
-      setLoggedIn(true);
-      setUserId(response.userID);
-      setName(response.name);
-      setEmail(response.email);
-      setPicture(response.picture.data.url);
-    }
-    catch (err) {
-      console.log(err);
-    }
-  }
-  
-  const responseFacebook = response => {
-    console.log(response);
-    postData(response);
-  }
-
-  let fbContent = null;
-
-  if(loggedIn) {
-    //do things when logged in
-    fbContent=(
-      <div>
-        <img src={picture} alt={name} />
-        <h2>Welcome {name}!</h2>
-        Email: {email}
-      </div>
-    )
-  }
-  else {
-    fbContent = (<FacebookLogin
+  return (
+    <div className={FacebookLogin}>{<FacebookLogin
       appId="3192124317669140"
       autoLoad={false}
       fields="name,email,picture"
       onClick={componentClicked}
-      callback={responseFacebook} />)
-  }
-
-  return (
-    <div>{fbContent}</div>
+      callback={responseFacebook} />}</div>
   )
 }
