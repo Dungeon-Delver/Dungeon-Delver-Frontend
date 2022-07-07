eae5a556262e2b7d480e367aab0f3e6c8c4f65a6
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-07 15:54:41 -0700

    Implemented NavBar

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+import "./Logo.css"
+import imageSource from "../../images/Icon.png";
+
+export default function Logo() {
+    return (
+        <div className="logo">
+            <Link to="/"><img className="logo-img" src={imageSource} alt="Return Home"/></Link>
+        </div>
+
+    )
+  }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import * as React from 'react'
+import {Link} from "react-router-dom"
+
 import "./Navbar.css"
+
+import Logo from "../Logo/Logo"
+
 
 export default function Navbar({handleLogout}) {
   return (
     <div className="navbar">
+      <Logo id="logo" />
+      <Link to={`/create-party`}><button className="create-party-button">Create Party</button></Link>
+      <Link to={`/find-parties`}><button className="find-parties-button">Find Parties</button></Link>
+      <Link to={`/parties`}><button className="my-parties-button">My Parties</button></Link>
       <button className="logout-button" onClick={() => {handleLogout()}}>Logout</button>
     </div>
   )
