bcaf90fde3de68728fab964e4b4a1f74c663446d
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-07 17:01:43 -0700

    Refactored code to use recoil atoms

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import {
+  RecoilRoot,
+} from 'recoil';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
