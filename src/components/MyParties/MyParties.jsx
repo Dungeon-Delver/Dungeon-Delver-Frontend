2393824959ce2a592d21a3ad9e7c462d159ef99e
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-12 17:14:48 -0700

    Basic Setup to get Parties from Backend

--- src/components/MyParties/MyParties.jsx
+++ src/components/MyParties/MyParties.jsx
@@ -5,6 +5,27 @@ import './MyParties.css'
 import Constants from '../../constants/appConstants'
 
 export default function MyParties() {
+  const URL = Constants().URL;
+  const [parties, setParties] = React.useState([]);
+  const [error, setError] = React.useState("");
+  const getCurrentUser = Constants().getCurrentUser;
+
+  React.useEffect( () => {
+    const getParties = async () => {
+      try{
+        const user = await getCurrentUser();
+        const res = await axios.get(`${URL}user/${user.id}/parties`)
+        console.log('res: ', res);
+      }
+      catch (err) {
+        console.log(err)
+        setError(err);
+      }
+      
+    }
+    getParties()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="my-parties">
