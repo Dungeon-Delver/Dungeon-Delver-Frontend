70cb515374f718741a23655d2092d943e18f19e3
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-12 18:15:46 -0700

    Implementing Party Card
    Work in progress

--- src/components/MyParties/MyParties.jsx
+++ src/components/MyParties/MyParties.jsx
@@ -5,13 +5,16 @@ import './MyParties.css'
 import Constants from '../../constants/appConstants'
 import Loader from '../Loader/Loader'
 import PartyCard from "../PartyCard/PartyCard.jsx"
+import { useRecoilValue } from 'recoil'
+import { currentUser } from '../../recoil/atoms/atoms'
 
 export default function MyParties() {
   const URL = Constants().URL;
-  const [parties, setParties] = React.useState(["hi"]);
+  const [parties, setParties] = React.useState([]);
   const [error, setError] = React.useState("");
   const [loading, setLoading] = React.useState(false);
   const getCurrentUser = Constants().getCurrentUser;
+  const curUser = useRecoilValue(currentUser)
 
   React.useEffect( () => {
     const getParties = async () => {
@@ -48,12 +51,16 @@ export default function MyParties() {
       </div>
     )
   }
+  if(parties.length === 0) {
+    return(<Loader />)
+  }
+
   return (
     <div className="my-parties">
       <Link to={`/create-party`}><button className="create-party-button">Create a Party</button></Link>
       <div className="parties">
         {parties.map((item, i) => {
-          return <PartyCard key={`${i}${item.objectId}`} party={item} />
+          return <PartyCard key={`${i}${item.objectId}`} party={item} role={item.dm.objectId===curUser.id ? "Dungeon Master" : "Player"}/>
         })}
       </div>
     </div>
--- src/components/PartyCard/PartyCard.jsx
+++ src/components/PartyCard/PartyCard.jsx
@@ -1,9 +1,28 @@
 import * as React from 'react'
 import "./PartyCard.css"
+import Keys from "../../keys.json"
+import Parse from 'parse/dist/parse.min.js';
 
-export default function PartyCard({party}) {
+Parse.initialize(Keys.parse.appId, Keys.parse.javascriptKey)
+Parse.serverURL = 'https://parseapi.back4app.com';
 
+export default function PartyCard({party, role}) {
+  const [dm, setDm] = React.useState("")
+  const [players, setPlayers] = React.useState("")
+
+  React.useEffect(() => {
+    //Use parse to find dm, players of party
+  })
   return (
-    <div className="party-card">{party.name}</div>
+    <div className="party-card">
+      <div className="party-image">
+        <img src={party.image} alt={party.name}/>
+      </div>
+      <div className="party-title">{party.name}</div>
+      <div className="role">{role}</div>
+      <div className="members">
+        <div className="dm">{}</div>
+      </div>
+      </div>
   )
 }
