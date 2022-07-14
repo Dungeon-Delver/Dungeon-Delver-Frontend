d4b85a448f28f22151486e00b05b2ad5037c9402
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-14 15:03:50 -0700

    Refactored code to make button it's own component

--- src/components/PartyPanel/PartyPanel.jsx
+++ src/components/PartyPanel/PartyPanel.jsx
@@ -6,14 +6,51 @@ import axios from 'axios'
 import Constants from '../../constants/appConstants'
 
 export default function PartyPanel({party, inParty}) {
-  const [buttonText, setButtonText] = React.useState("")
+
+  return (
+    <div className="party-panel">
+      <MembersList party={party} visible={!inParty && (party.status==="Closed")} maxDisplay={-1} />
+      <CategoriesDisplay party={party} />
+      <PanelButton party={party} inParty={inParty}/>
+    </div>
+  )
+}
+
+function PanelButton({party, inParty}) {
+  const [buttonText, setButtonText] = React.useState("Loading...")
   const [buttonDisabled, setButtonDisabled] = React.useState(false)
   const [onPress, setOnPress] = React.useState(() => {})
+
+  const [error, setError] = React.useState("");
 
   const URL = Constants().URL;
   const getCurrentUser = Constants().getCurrentUser;
 
   React.useEffect(() => {
+    const haveRequested = async () => {
+      try {
+        const response = await axios.get(`${URL}party/${party.objectId}/requested`)
+        const users = response.data.users
+        const userIds = users.map((item) => {
+          return item.objectId
+        })
+        console.log('userIds: ', userIds);
+        const currentUser = await getCurrentUser();
+        if(userIds.includes(currentUser.id)) {
+          setButtonDisabled(true)
+          setButtonText("Request Sent")
+        }
+        else {
+          setButtonText("Request to Join")
+          setOnPress(() => requestJoin)
+        }
+      }
+      catch (err) {
+        setError(err);
+        console.log(err);
+      }      
+    }
+
     if(!inParty && (party.status==="Closed"||party.full)) {
       setButtonText("Party Closed")
       setButtonDisabled(true)
@@ -25,8 +62,7 @@ export default function PartyPanel({party, inParty}) {
       setButtonText("Delete Party")
     }
     else {
-      setButtonText("Request to Join")
-      setOnPress(() => requestJoin)
+      haveRequested();
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -37,14 +73,17 @@ export default function PartyPanel({party, inParty}) {
       await axios.post(`${URL}user/${party.objectId}/join`, {userId: currentUser})
     }
     catch (err) {
+      setError(err);
       console.log(err)
     }
   }
-  return (
-    <div className="party-panel">
-      <MembersList party={party} visible={!inParty && (party.status==="Closed")} maxDisplay={-1} />
-      <CategoriesDisplay party={party} />
-      <button disabled={buttonDisabled} onClick={() => onPress()}>{buttonText}</button>
+
+  return(
+    <div className="panel-button"><button disabled={buttonDisabled} onClick={() => onPress()}>{buttonText}</button>
+      {error !=="" ?<div className="party-page-failed"> 
+        <h1 className="party-page-failed-message">{error.response.data ? error.response.data.error.message : error.message}</h1>
+        <h2 className="party-page-failed-status-text">{error.statusText}</h2>
+      </div> :""}
     </div>
   )
 }
