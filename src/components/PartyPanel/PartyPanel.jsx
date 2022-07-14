84e2e5936e0a9228cb87321e75b33b0614ce689c
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-14 14:33:26 -0700

    Frontend for Submitting Join Request

--- src/components/PartyPanel/PartyPanel.jsx
+++ src/components/PartyPanel/PartyPanel.jsx
@@ -2,10 +2,16 @@ import * as React from 'react'
 import "./PartyPanel.css"
 import MembersList from "../MembersList/MembersList"
 import CategoriesDisplay from "../CategoriesDisplay/CategoriesDisplay.jsx"
+import axios from 'axios'
+import Constants from '../../constants/appConstants'
 
 export default function PartyPanel({party, inParty}) {
-  const [buttonText, setButtonText] = React.useState("");
+  const [buttonText, setButtonText] = React.useState("")
   const [buttonDisabled, setButtonDisabled] = React.useState(false)
+  const [onPress, setOnPress] = React.useState(() => {})
+
+  const URL = Constants().URL;
+  const getCurrentUser = Constants().getCurrentUser;
 
   React.useEffect(() => {
     if(!inParty && (party.status==="Closed"||party.full)) {
@@ -20,15 +26,25 @@ export default function PartyPanel({party, inParty}) {
     }
     else {
       setButtonText("Request to Join")
+      setOnPress(() => requestJoin)
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const requestJoin = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      await axios.post(`${URL}user/${party.objectId}/join`, {userId: currentUser})
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div className="party-panel">
       <MembersList party={party} visible={!inParty && (party.status==="Closed")} maxDisplay={-1} />
       <CategoriesDisplay party={party} />
-      <button disabled={buttonDisabled}>{buttonText}</button>
+      <button disabled={buttonDisabled} onClick={() => onPress()}>{buttonText}</button>
     </div>
   )
 }
