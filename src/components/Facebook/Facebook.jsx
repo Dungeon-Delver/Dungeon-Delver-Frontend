19faf1401bb3c52c8dc5e8427a81811bac351084
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-14 17:12:44 -0700

    Login Working Again (had to restart chrome for some reason)

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -54,7 +54,6 @@ export default function FacebookOAuth() {
           // logIn returns the corresponding ParseUser object
           const user = await getCurrentUser();
           localStorage.setItem('user', JSON.stringify(user));
-          console.log(localStorage.getItem('user'))
           if(!user.get("enabled")) {
             console.log("disabled user")
             setLoggedIn("disabled")
