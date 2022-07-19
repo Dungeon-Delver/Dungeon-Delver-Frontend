15bd9838e9dba1dfaf97b854c939666d9f77124e
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-18 19:36:38 -0700

    CSS for accepting/rejecting users from party

--- src/components/PartyPanelMembersList/PartyPanelMembersList.jsx
+++ src/components/PartyPanelMembersList/PartyPanelMembersList.jsx
@@ -19,7 +19,7 @@ export default function MembersList({players, dm, inParty}) {
           return <li key={i} className="player">
               <div className="player-picture member-picture"><img src={item.picture} alt={item.name}/></div>
               <div className="player-name member-name">{item.username}</div>
-              {inParty==="dm" ? <button><span class='text'>Remove User</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button> : ""}
+              {inParty==="dm" ? <button><span className='text'>Remove User</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button> : ""}
             </li>
         })}
       </ul>
