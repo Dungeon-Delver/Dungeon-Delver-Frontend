ba4ad9ca6cd3dc4482564e6beef5ff42020839e1
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-18 17:08:23 -0700

    Revert testing button changes

--- src/components/PartyPanelMembersList/PartyPanelMembersList.jsx
+++ src/components/PartyPanelMembersList/PartyPanelMembersList.jsx
@@ -11,16 +11,15 @@ export default function MembersList({players, dm, inParty}) {
   return (
     <ul className={classNames({"panel-party-members": true, "panel-party-members-dm": inParty==="dm"})}>
       <h2 className="party-members-heading">Current Members</h2>
-        <li className="player">
+        <li className="dm">
           <div className="dm-picture member-picture"><img src={dm.picture} alt={dm.name}/></div>
           <div className="dm-name member-name">👑 {dm.username}</div>
-          {inParty==="dm" ? <button><span class='text'>Remove User</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button> : ""}
         </li>
         {players.map((item, i) => {
           return <li key={i} className="player">
               <div className="player-picture member-picture"><img src={item.picture} alt={item.name}/></div>
               <div className="player-name member-name">{item.username}</div>
-              {inParty==="dm" ? <button><span>Remove User</span></button> : ""}
+              {inParty==="dm" ? <button><span class='text'>Remove User</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button> : ""}
             </li>
         })}
       </ul>
