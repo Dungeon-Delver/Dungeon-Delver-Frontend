5a1fa0a6f82a96cba59d7f5f899efb50ea03b4c7
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-22 11:17:58 -0700

    Import: socket-io-client

--- src/components/PartyChat/PartyChat.jsx
+++ src/components/PartyChat/PartyChat.jsx
@@ -2,10 +2,15 @@ import * as React from 'react'
 import "./PartyChat.css"
 
 //Stretch
-export default function PartyChat({party}) {
+export default function PartyChat({party, inParty}) {
+  if(inParty!=="dm" && inParty!=="player") {
+    return <div className="party-chat">
+      <h1>Join the party to see the chat!</h1>
+    </div>
+  }
   return (
     <div className="party-chat">
-
+      
     </div>
   )
 }
