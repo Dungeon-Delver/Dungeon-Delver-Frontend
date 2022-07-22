2281f976a1baf7bf4408b15c40914a0e6eb975f1
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-22 12:01:03 -0700

    Frontend Chat Setup

--- src/components/PartyChat/PartyChat.jsx
+++ src/components/PartyChat/PartyChat.jsx
@@ -1,16 +1,51 @@
-import * as React from 'react'
+import {useState} from 'react'
 import "./PartyChat.css"
+import useChat from "../../hooks/useChat"
+import classNames from 'classnames'
 
 //Stretch
 export default function PartyChat({party, inParty}) {
+  const partyId = party.party.objectId
+  const {messages, sendMessage} = useChat(partyId)
+  const [newMessage, setNewMessage] = useState("")
+
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  }
+
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage("");
+  };
+
+
   if(inParty!=="dm" && inParty!=="player") {
     return <div className="party-chat">
       <h1>Join the party to see the chat!</h1>
     </div>
   }
+
+
   return (
     <div className="party-chat">
-      
+      <ol className="messages-list">
+        {messages.map((message, i) => {
+          <li
+          key={i}
+          className={classNames({"message-item": true, "my-message": message.ownedByCurrentUser, "received-message": !message.ownedByCurrentUser})}>
+            {message.body}
+          </li>
+        })}
+      </ol>
+      <textarea
+        value={newMessage}
+        onChange={handleNewMessageChange}
+        placeholder="Write message..."
+        className="new-message-input-field"
+      />
+      <button onClick={handleSendMessage} className="send-message-button">
+        Send
+      </button>
     </div>
   )
 }
