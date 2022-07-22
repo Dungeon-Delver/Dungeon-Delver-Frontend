3a560bde63d384ea168f6a8866377841060aa381
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-22 15:51:54 -0700

    Update: Display user information
    Includes Basic party chat css

--- src/components/PartyChat/PartyChat.jsx
+++ src/components/PartyChat/PartyChat.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import "./PartyChat.css"
 import useChat from "../../hooks/useChat"
 import classNames from 'classnames'
+import userEvent from '@testing-library/user-event'
 
 //Stretch
 export default function PartyChat({party, inParty}) {
@@ -21,12 +22,6 @@ export default function PartyChat({party, inParty}) {
   };
 
 
-  if((inParty!=="dm" && inParty!=="player")&&party.status!=="Public") {
-    return (<div className="party-chat">
-      <h1>Only party members can see the chat!</h1>
-    </div>)
-  }
-
   const handleKeyDown = (event) => {
     if(event.key === "Enter" && !event.shiftKey) {
       event.preventDefault()
@@ -35,15 +30,18 @@ export default function PartyChat({party, inParty}) {
   }
 
 
+  if((inParty!=="dm" && inParty!=="player")&&party.status!=="Public") {
+    return (<div className="party-chat">
+      <h1>Only party members can see the chat!</h1>
+    </div>)
+  }
+
+
   return (
     <div className="party-chat">
       <ol className="messages-list">
         {messages.map((message, i) => {
-          return (<li
-          key={i}
-          className={classNames({"message-item": true, "my-message": message.ownedByCurrentUser, "received-message": !message.ownedByCurrentUser})}>
-            {message.body}
-          </li>)
+          return(<ChatMessage key={i} message={message} prevMessage={i === 0 ? true : messages[i-1]}/>)
         })}
       </ol>
       {inParty==="dm" || inParty==="player" ? <><textarea
@@ -58,3 +56,14 @@ export default function PartyChat({party, inParty}) {
     </div>
   )
 }
+
+function ChatMessage({message, prevMessage}) {
+  const newSender = prevMessage===true || prevMessage.senderId !== message.senderId
+  return (<li
+    className={classNames({"message-item": true, "my-message": message.ownedByCurrentUser, "received-message": !message.ownedByCurrentUser, "new-sender" : newSender})}>
+      {newSender ?
+        <><div className="chat-img-container"><img src={message.user.picture} alt={message.user.name} /></div><div className="chat-user">{message.user.username}</div></>
+      : ""}
+      <div className="message-body">{message.body}</div>
+    </li>)
+  }
