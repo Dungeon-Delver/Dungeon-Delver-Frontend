2831560fb1201b9e807ad7873394c0c9782001c4
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-07-28 13:37:21 -0700

    Notification Design Complete

--- src/components/NotificationCard/NotificationCard.jsx
+++ src/components/NotificationCard/NotificationCard.jsx
@@ -1,16 +1,19 @@
+import classNames from "classnames";
+import { Link } from "react-router-dom";
 import "./NotificationCard.css"
 
-export default function NotificationCard( {notification} ) {
+export default function NotificationCard( {notification, handleCloseNavbar, unread} ) {
   let message;
   if(notification.type === "remove") {
     message = `You were removed from ${notification.party.name} by ${notification.sourceUser.username}`
   }
   return (
-    <div className="notification-card">
-      <div className="info">
+    <Link to={`/party/${notification.party.objectId}`}>
+      <div className={classNames({"notification-card": true, unread: unread, read: !unread})} onClick={() => handleCloseNavbar(false)}>
+        <span className="dot"></span>
         <img className="notification-img" src={notification.sourceUser.picture} alt="notification" />
         <span>{message}</span>
       </div>
-    </div>
+    </Link>
   )
 }
--- src/components/NotificationCenter/NotificationCenter.jsx
+++ src/components/NotificationCenter/NotificationCenter.jsx
@@ -15,33 +15,46 @@ export default function NotificationCenter() {
   useNotification(unreadNotifications, setUnreadNotifications)
 
   const curUser = useRecoilValue(currentUser)
+
+  const handleCloseNavbar = () => {
+    setNotificationsOpen(false)
+    setNotifications([...unreadNotifications, ...notifications])
+    setUnreadNotifications([])
+    try {
+      axios.post(`${BACKEND_URL}user/read-notifications`, {notifications: unreadNotifications})
+    }
+    catch (err) {
+      console.error(err)
+    }
+  }
   
   useEffect(() => {
     const getNotifications = async () => {
       try {
         const response = await axios.get(`${BACKEND_URL}user/${curUser.id}/notifications`)
-        setNotifications(response.data.notifications)
+        setNotifications(response.data.notifications.readNotifications)
+        setUnreadNotifications(response.data.notifications.unreadNotifications)
       }
       catch (err) {
         console.error(err)
       }
     }
     getNotifications()
-  }, [curUser.objectId])
+  }, [curUser.id])
 
   return (
     <div className="notification-center">
       <div className="notification-bell">
-        <img src={NotificationBell} className="iconImg" alt="notification bell" onClick={() => setNotificationsOpen(!notificationsOpen)}/>
+        <img src={NotificationBell} className="iconImg" alt="notification bell" onClick={() => {!notificationsOpen ? setNotificationsOpen(true) : handleCloseNavbar()}}/>
         {unreadNotifications.length > 0 ? <div className="counter">{unreadNotifications.length}</div> : ""}
       </div>
       {notificationsOpen ? 
             <ol className="notifications-menu">
               {unreadNotifications.map((item, i) => {
-                return <NotificationCard key={i} notification={item} />
+                return <NotificationCard key={i} notification={item} handleCloseNavbar={handleCloseNavbar} unread={true}/>
               })}
               {notifications.map((item, i) => {
-                return <NotificationCard key={i} notification={item} />
+                return <NotificationCard key={i} notification={item} handleCloseNavbar={handleCloseNavbar} unread={false}/>
               })}
             </ol>
         : ""}
