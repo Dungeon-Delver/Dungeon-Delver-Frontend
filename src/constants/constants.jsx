98f4d7a4e263dcbb165c5f31f8048dead26e7c45
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-08-09 16:22:44 -0700

    Update: update constants to post to heroku app

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "http://localhost:3001/";
+export const BACKEND_URL = "https://murmuring-taiga-17326.herokuapp.com/"
 
-export const SOCKET_SERVER_URL = "http://localhost:3002";
+export const SOCKET_SERVER_URL = "https://murmuring-taiga-17326.herokuapp.com/";
 
 export const FILE_HOST_URL = "https://api.imgbb.com/1/upload";
