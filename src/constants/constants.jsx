56ebf47feb4a90aa5ac1abafc3ff5ffb5ed60c89
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2023-01-16 09:11:55 -0500

    Redirect: Pointed server to new backend

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "https://murmuring-taiga-17326.herokuapp.com/"
+export const BACKEND_URL = "https://zircon-grandiose-individual.glitch.me/"
 
-export const SOCKET_SERVER_URL = "https://murmuring-taiga-17326.herokuapp.com/";
+export const SOCKET_SERVER_URL = "https://zircon-grandiose-individual.glitch.me/";
 
 export const FILE_HOST_URL = "https://api.imgbb.com/1/upload";
